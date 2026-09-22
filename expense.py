74e505e63bc0035427d9155a62d4f119fcdbfff2
naveenya <mohanrajnaveenya@gmail.com>
2026-09-22 16:34:01 +0530

    replaced os and json with sqlite3

--- expense.py
+++ expense.py
@@ -1,122 +1,82 @@
 import argparse
-import os
-import json
-
-file="expense.json"
-
-def load_expense():
-    if os.path.exists(file):
-        with open(file,"r") as f:
-            return json.load(f)
-    return[]
-
-expense=load_expense()
-
-def save_expense():
-    with open(file,"w") as f:
-        json.dump(expense,f)
-
-def add(args):
-    expense1={}
-    expense1["name"]=args.name
-    expense1["price"]=args.price
-    expense.append(expense1)
-    save_expense()
-    
+import sqlite3
+#to connect to mysql
+def connect_sql():
+    return sqlite3.connect("expense.db")
+#to create a table
+def create_table():
+     conn=connect_sql()
+     cursor=conn.cursor()
+     cursor.execute(""" CREATE TABLE  IF NOT EXISTS expenses (id INT primary key   ,
+name varchar(50) ,
+expense int,
+category varchar(50) ,
+date_of_entry date)""")
+     conn.commit()
+     conn.close()
+#to insert a row
+def insert_row(id,name,expense,category,date_of_entry):
+    a=connect_sql()
+    b=a.cursor()
+    b.execute(""" insert into expenses (id,name,expense,category,date_of_entry)
+   values(?,?,?,?,?) """,(id,name,expense,category,date_of_entry))
+    a.commit()
+    a.close()
+#to update name
+def update_expense(expense,name):
+    a=connect_sql()
+    b=a.cursor()
+    b.execute(""" update expenses set expense=? where name=?""",
+    (expense,name))
+    a.commit()
+    a.close()
+def delete_expense(name):
+    a=connect_sql()
+    b=a.cursor()
+    b.execute("""delete from expenses where name=?""", (name,))
+    a.commit()
+    a.close()
 def view_expense():
-    for i in expense:
-        print("name",i["name"],"price",i["price"])
+    a=connect_sql()
+    b=a.cursor()
+    b.execute(""" select * from expenses""")
+    a=b.fetchall()
+    for i in a:
+        print(i)
+#argparse
+argparser=argparse.ArgumentParser()
+sub=argparser.add_subparsers(dest="command")
 
-def total():
-    t=0
-    for i in expense:
-        t+=i["price"]
-    print(t)
+#create row
+create_row=sub.add_parser("insert_row")
+create_row.add_argument("--id",type=int)
+create_row.add_argument("--name",required=True)
+create_row.add_argument("--category")
+create_row.add_argument("--expense",type=int)
+create_row.add_argument("--date_of_entry")
 
-def delete_item(args):
-    for i in expense:
-        if i["name"]==args.name:
-            expense.remove(i)
-            save_expense()
-            break
-    else:
-        print("item not found")
-    print(expense)
+update_expenses=sub.add_parser("update_expenses")
+update_expenses.add_argument("--expense",type=int,required=True)
+update_expenses.add_argument("--name",required=True)
 
-   
-def update_item(args):
-    for i in expense:
-        if i["name"]==args.name:
-            i["price"]=args.price
-            save_expense()
-            break
-    else :
-        print("item not found")
-    print(expense)
-    
-def search_expenses(args):
-    for i in expense:
-        if i["name"]==args.name:
-            print(i["price"])
-            break
-    else :
-        print("item not found")
-    print(expense)
+delete_expenses=sub.add_parser("delete_expenses")
+delete_expenses.add_argument("--name",required=True)
 
-def add_category(args):
-    for i in expense:
-        if i["name"]==args.pname:
-            i["categories"]=args.cname
-            save_expense()
-            break
-    else:
-        print("item not found")
-    print(expense)
-    
-a=argparse.ArgumentParser()
+view_expenses=sub.add_parser("view_expenses")
 
-sub=a.add_subparsers(dest="command")
+d=argparser.parse_args()
 
-#add product and expense
-add_product=sub.add_parser("add",help="add expenses")
-add_product.add_argument("--name",required=True,help="product name")
-add_product.add_argument("--price",type=int,required=True,help="product price")
 
-#view_expense
-view_expenses=sub.add_parser("view_expenses",help="view product and  expenses")
-
-#total
-total_expense=sub.add_parser("total_expense",help="total of expenses")
-#delete
-delete_item_=sub.add_parser("delete_item_",help="eneter product name you want to delete")
-delete_item_.add_argument("--name",required=True)
-
-#update price
-update_price=sub.add_parser("update_price",help="enter name of product to update price")
-update_price.add_argument("--name",required=True)
-update_price.add_argument("--price",required=True,type=int)
-
-#search_expenses
-search_name=sub.add_parser("search_name",help="enter product name to find expense")
-search_name.add_argument("--name",required=True)
-
-#add_category(args)
-add_new=sub.add_parser("add_new",help="add new category name ")
-add_new.add_argument("--pname",required=True)
-add_new.add_argument("--cname",required=True)
-
-args=a.parse_args()
-if args.command=="add":
-    add(args)
-elif args.command=="view_expenses":
+create_table()
+if d.command=="insert_row":
+    insert_row(d.id,d.name,d.expense,d.category,d.date_of_entry)
     view_expense()
-elif args.command=="total_expense":
-    total()
-elif args.command=="delete_item_":
-    delete_item(args)
-elif args.command=="update_price":
-    update_item(args)
-elif args.command=="search_name":
-    search_expenses(args)
-elif args.command=="add_new":
-    add_category(args)
+elif d.command=="update_expenses":
+    update_expense(d.expense,d.name)
+    view_expense()
+elif d.command=="delete_expenses":
+    delete_expense(d.name)
+    view_expense()
+elif d.command=="view_expenses":
+    view_expense()
+
